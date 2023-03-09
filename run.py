1f3908a67d851fe9e924e1b5db2526d894fdd8a4
helenahagman <helena@rapibo.se>
2023-03-09 08:23:38 +0000

    Created board and basic structure of functions

--- run.py
+++ run.py
@@ -1,3 +1,44 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class TicTacToeGame:
+
+    def create_board(self):
+        """
+        Creates the board for the game
+        3 rows and 3 columns
+        9 spaces total
+        """
+        for i in range(3):
+            row[]
+            for j in range(3):
+                row.append('-')
+                self.board.append(row) 
+    
+    def player_one():
+        """
+        Human player defined with X in the game
+        """
+
+    def computer_player()
+        """
+        Computer player defined with O in the game
+        """
+
+    def start():
+        """
+        Start the game and shows the board
+        """    
+    
+    def win():
+        """
+        Defines what happens when the game is won
+        A game is won when a player have all 3 markers in a row
+        """
+    
+    def board_full():
+        """
+        When both players used their initial 3 marks
+        the game continues but now the players need to move a marker on the board to a different space
+        """
+
+print()
